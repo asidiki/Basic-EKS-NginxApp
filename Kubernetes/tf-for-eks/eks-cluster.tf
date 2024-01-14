29569008ce0fec2316a9fff4503dc901e5951c29
Ansab Sidiki <ansab@sidiki.io>
2024-01-13 21:27:38 -0500

    fixed jenkinsfile

--- Kubernetes/tf-for-eks/eks-cluster.tf
+++ Kubernetes/tf-for-eks/eks-cluster.tf
@@ -7,8 +7,8 @@ module "eks" {
 
     cluster_endpoint_public_access  = true
 
-    vpc_id = module.myjenkins-server-vpc.vpc_id
-    subnet_ids = module.myjenkins-server-vpc.private_subnets
+    vpc_id = module.nginxapp-vpc.vpc_id
+    subnet_ids = module.nginxapp-vpc.private_subnets
 
     tags = {
         environment = "development"
