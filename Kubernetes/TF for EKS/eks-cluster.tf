578541b9c3707dc115e67606ce95ea8461ae962b
Ansab Sidiki <ansab@sidiki.io>
2024-01-13 21:06:22 -0500

    tf code for eks cluster

---
+++ Kubernetes/TF for EKS/eks-cluster.tf
@@ -0,0 +1,27 @@
+#eks-cluster.tf
+module "eks" {
+    source  = "terraform-aws-modules/eks/aws"
+    version = "~> 19.0"
+    cluster_name = "nginxapp-eks-cluster"
+    cluster_version = "1.28"
+
+    cluster_endpoint_public_access  = true
+
+    vpc_id = module.myjenkins-server-vpc.vpc_id
+    subnet_ids = module.myjenkins-server-vpc.private_subnets
+
+    tags = {
+        environment = "development"
+        application = "nginxapp-server"
+    }
+
+    eks_managed_node_groups = {
+        dev = {
+            min_size = 1
+            max_size = 3
+            desired_size = 2
+
+            instance_types = ["t2.small"]
+        }
+    }
+}
